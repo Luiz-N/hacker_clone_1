378cdd099b474b8133f56cc31b3ff462334fba45
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-04 17:53:50 -0500

    update / delete post

--- app/controllers/post.rb
+++ app/controllers/post.rb
@@ -12,6 +12,16 @@ get '/post/:id' do
   erb :post_page
 end
 
+get '/post/:id/edit' do
+  @post = Post.find(params[:id])
+
+  if @post.author_id == session[:user_id]
+    erb :edit_post
+  else
+    redirect "/post/#{params[:id]}"
+  end
+
+end
 
 
 # POST ####################
@@ -22,5 +32,17 @@ post '/posts' do
   @post.author_id = session[:user_id]
   @post.save
 
+  redirect "/"
+end
+
+post '/post/:id' do
+  @post = Post.find(params[:id])
+  @post.update_attributes(params[:post])
+  redirect "/"
+end
+
+post '/post/:id/delete' do
+  @post = Post.find(params[:id])
+  @post.destroy
   redirect "/"
 end
